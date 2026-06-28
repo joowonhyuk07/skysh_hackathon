5aed79adcf3b6f7837a2f37d95ca1fc880e6b1b9
joowonhyuk07 <jwh070607@gmail.com>
2026-06-28 10:14:38 +0900

    add procfile

--- app.py
+++ app.py
@@ -156,4 +156,5 @@ def index():
         return f.read(), 200, {'Content-Type': 'text/html'}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
